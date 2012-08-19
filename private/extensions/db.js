51db2732cf1bca0ae698a04173396d6503bc886a
s-p-n <spence@wysgui.com>
2012-08-19 10:37:32 -0600

    fixing db extension

--- private/extensions/db.js
+++ private/extensions/db.js
@@ -1,4 +1,4 @@
-var db = require('mongojs').connect('rakr', ['clients']);
+var db = require('mongojs').connect('DB_NAME', ['Collection1', 'Collection2']);
 module.exports = function (m) {
 	m.db = db;
 };
